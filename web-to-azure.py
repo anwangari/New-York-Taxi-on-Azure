1d399a7b70958d47bbc8118e5eab04fdc5654d2f
Anthony Njuguna <98034679+anwangari@users.noreply.github.com>
2024-12-03 13:13:47 +0300

    Create web-to-azure.py
    This function fetches files from a web URL and uploads them directly to Azure Data Lake Storage Gen2

---
+++ web-to-azure.py
@@ -0,0 +1,54 @@
+!pip install azure-storage-file-datalake azure-identity
+!pip install python-dotenv
+
+from dotenv import load_dotenv
+import os
+import requests
+from azure.storage.blob import BlobServiceClient
+import requests
+
+# read environment varibles from memory
+load_dotenv()
+
+STORAGE_ACCOUNT_URL = os.getenv("STORAGE_ACCOUNT_URL")
+STORAGE_ACCOUNT_KEY = os.getenv("CREDENTIAL")
+CONTAINER = os.getenv("CONTAINER_NAME")
+
+def upload_web_data_to_azure(storage_account_url, container_name, credential):
+    # connect to data store and create container
+    blob_service_client = BlobServiceClient(
+        account_url=STORAGE_ACCOUNT_URL, credential=STORAGE_ACCOUNT_KEY
+    )
+    try:
+        container_client = blob_service_client.get_container_client(CONTAINER)
+        if not container_client.exists():
+            container_client.create_container()
+            print(f"Container '{container_name}' created.")
+    except Exception as e:
+        print(f"Error accessing or creating container: {e}")
+        return
+
+    # iTERATE THOUGH SERVICES AND MONTHS    
+    for service in ['yellow', 'green']:
+      for i in range(1,13):
+          month = f"0{i}"
+          month = month[-2:]
+          base_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/"
+          f_name = f"{service}_tripdata_2019-{month}.parquet"
+          file_url = f"{base_url}{f_name}"
+
+          try:
+            # Download the file from the web
+            response = requests.get(file_url, stream=True)
+            response.raise_for_status()  # Raise an error for bad status codes
+                
+            # Upload the file to Azure
+            blob_client = container_client.get_blob_client(f_name)
+            blob_client.upload_blob(response.content, overwrite=True)
+            
+            print(f"Uploaded: {f_name}")
+          except Exception as e:
+            print(f"Failed to upload {f_name}: {e}")
+
+    return "Upload complete"
+
